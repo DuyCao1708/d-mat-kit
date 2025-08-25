778eea30cfed3cbb761decda3e3f9da3d6b30694
DuyCao1708 <tucaoduygl@gmail.com>
2025-08-25 17:25:46 +0700

    #3 - DuyCao1708 installed angular material
    Setup basic layout with sidenav and navbar

--- apps/d-mat-kit-presentation/src/app/app.ts
+++ apps/d-mat-kit-presentation/src/app/app.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Navbar } from './layout/navbar/navbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Navbar, MatSidenavModule, MatButtonModule],
   selector: 'app-root',
-  template: ``,
+  template: ` <navbar></navbar>
+    <mat-sidenav-container
+      class="example-sidenav-container"
+      [style.marginTop.px]="isMobile() ? 56 : 0"
+    >
+      <mat-sidenav
+        #snav
+        [mode]="isMobile() ? 'over' : 'side'"
+        [fixedInViewport]="isMobile()"
+        fixedTopGap="56"
+      >
+      </mat-sidenav>
+
+      <mat-sidenav-content>
+        <button matButton (click)="snav.toggle()">toggle sidenav</button>
+      </mat-sidenav-content>
+    </mat-sidenav-container>`,
+  styles: [
+    `
+      :host {
+        display: flex;
+        flex-direction: column;
+        position: absolute;
+        inset: 0;
+      }
+    `,
+  ],
 })
 export class App {
+  isMobile = toSignal(
+    inject(BreakpointObserver)
+      .observe(Breakpoints.Handset)
+      .pipe(map((resuslt) => resuslt.matches)),
+    { initialValue: false }
+  );
 }
---
+++ apps/d-mat-kit-presentation/src/app/layout/navbar/navbar.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  imports: [],
+  template: `
+    <p>
+      navbar works!
+    </p>
+  `,
+  styles: ``
+})
+export class Navbar {
+
+}
